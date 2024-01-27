44c8e541fe66f7986235f6f4d9e081b67f82a5fc
Vamsi Kodavanty <robotitans21518@gmail.com>
2024-01-27 00:07:05 -0800

    Arm test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/TestArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/TestArm.java
@@ -1,24 +1,26 @@
 package org.firstinspires.ftc.teamcode.opmode;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @Config
-@TeleOp(name="TestClaw", group="Linear OpMode")
-public class TestClaw extends LinearOpMode {
-    private Servo claw;
+@TeleOp(name="TestArm", group="Linear OpMode")
+public class TestArm extends LinearOpMode {
+    private Motor arm;
 
-    public static double clawPosition = 0;
+    public static double power = 0;
     @Override
     public void runOpMode() {
-        claw = hardwareMap.get(Servo.class, "leftClaw");
+        arm = new Motor(hardwareMap, "arm", Motor.GoBILDA.RPM_312);
+        arm.setZeroPowerBehavior(Motor.ZeroPowerBehavior.FLOAT);
 
         waitForStart();
 
         while (!isStopRequested()) {
-            claw.setPosition(clawPosition);
+            arm.set(power);
         }
     }
 }
